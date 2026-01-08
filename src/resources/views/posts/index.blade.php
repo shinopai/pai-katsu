0f6eda8ae4e08af40eec7c54d965fee23f8c772d
shinopai <harbor.shinozaki@gmail.com>
2026-01-08 16:31:42 +0900

    サイドバーをパーシャル化、新規投稿作成ボタンをコンポネント化

--- src/resources/views/posts/index.blade.php
+++ src/resources/views/posts/index.blade.php
@@ -4,90 +4,17 @@
   <div class="top">
     <div class="wrap">
       <div class="top__content flex">
-        <aside class="top__sidebar">
-          <!-- ZOOM朝活 -->
-          <div class="top__sidebar-zoom">
-            <a href="#" class="top__sidebar-zoom-link">
-              ZOOM朝活
-            </a>
-          </div>
+        {{-- サイドバー --}}
+        @include('partials.sidebar')
 
-          <!-- タグ一覧 -->
-          <div class="top__sidebar-tags">
-            <div class="top__sidebar-tags-header">
-              <span class="top__sidebar-tags-title">メインタグ</span>
-            </div>
-
-            <ul class="top__sidebar-tags-list">
-              @foreach ($mainTags as $tag)
-                <li class="top__sidebar-tags-item">
-                  <a href="{{ route('tags.show', ['tag' => $tag->id]) }}"
-                    class="top__sidebar-tags-link">#{{ $tag->name }}</a>
-                </li>
-              @endforeach
-            </ul>
-          </div>
-
-          <!-- ランキング -->
-          <div class="top__sidebar-ranking">
-            <div class="top__sidebar-ranking-header">
-              <span class="top__sidebar-ranking-title">早起き達成ランキング</span>
-              <span class="top__sidebar-ranking-month">1月</span>
-            </div>
-
-            <ul class="top__sidebar-ranking-list">
-              <li class="top__sidebar-ranking-item">
-                <span class="top__sidebar-ranking-rank">1</span>
-                <span class="top__sidebar-ranking-name">ゲストユーザーさん</span>
-                <span class="top__sidebar-ranking-count">
-                  <span class="top__sidebar-ranking-number">5</span>
-                  日
-                </span>
-              </li>
-              <li class="top__sidebar-ranking-item">
-                <span class="top__sidebar-ranking-rank">2</span>
-                <span class="top__sidebar-ranking-name">ゴリさん</span>
-                <span class="top__sidebar-ranking-count">
-                  <span class="top__sidebar-ranking-number">4</span>
-                  日
-                </span>
-
-              </li>
-              <li class="top__sidebar-ranking-item">
-                <span class="top__sidebar-ranking-rank">2</span>
-                <span class="top__sidebar-ranking-name">soraさん</span>
-                <span class="top__sidebar-ranking-count">
-                  <span class="top__sidebar-ranking-number">4</span>
-                  日
-                </span>
-              </li>
-              <li class="top__sidebar-ranking-item">
-                <span class="top__sidebar-ranking-rank">3</span>
-                <span class="top__sidebar-ranking-name">ケンさん</span>
-                <span class="top__sidebar-ranking-count">
-                  <span class="top__sidebar-ranking-number">3</span>
-                  日
-                </span>
-              </li>
-              <li class="top__sidebar-ranking-item">
-                <span class="top__sidebar-ranking-rank">4</span>
-                <span class="top__sidebar-ranking-name">daiさん</span>
-                <span class="top__sidebar-ranking-count">
-                  <span class="top__sidebar-ranking-number">2</span>
-                  日
-                </span>
-              </li>
-            </ul>
-          </div>
-        </aside>
+        {{-- 投稿一覧 --}}
         <main class="top__posts" id="xTopPosts" data-load-url="/posts/load">
           @include('posts._list', ['posts' => $posts])
         </main>
       </div>
     </div>
-    <a href="{{ route('posts.create') }}" class="top__new-post flex">
-      <i class="fa-solid fa-plus top__new-post-icon" style="color: #ffffff;"></i>
-      <span class="top__new-post-text">新規投稿</span>
-    </a>
+
+    {{-- 新規投稿ボタン --}}
+    @include('components.create_post_button')
   </div>
 @endsection
