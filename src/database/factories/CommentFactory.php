5ab39819c90d50fb1de6d88ce75ab056ccd63a1b
shinopai <harbor.shinozaki@gmail.com>
2026-01-10 10:48:09 +0900

    コメントテストデータ作成

---
+++ src/database/factories/CommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->value('id'),
+            'post_id' => Post::inRandomOrder()->value('id'),
+            'content' => $this->faker->realText(80)
+        ];
+    }
+}
